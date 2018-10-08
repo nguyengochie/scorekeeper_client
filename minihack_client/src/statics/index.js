2c9b459cc77dd9bd0a308b48ebb9e5560cbad0c9
Nguyen Ngoc Hieu <nguyengoc.hie@gmail.com>
2018-10-08 21:24:45 +0700

    lan3

--- minihack_client/src/statics/index.js
+++ minihack_client/src/statics/index.js
@@ -1,1 +1,2 @@
-export const API_ROOT = process.env.PORT ? 'https://score-keeper-hieu.herokuapp.com' :'https://localhost:6969';
+export const API_ROOT = process.env.ENVIROMENT == 'local' ? "http://localhost:6969" : "https://score-keeper-hieu.herokuapp.com";
+
